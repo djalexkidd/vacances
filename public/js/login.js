f2bae1836c3e6e623edd852c4e90bab9388ebb48
djalexkidd <alexpyr@outlook.fr>
2022-03-15 14:38:08 +0100

    Login plus que fonctionnel

--- public/js/login.js
+++ public/js/login.js
@@ -4,6 +4,16 @@ const emailField = document.querySelector(".email")
 const passwordField = document.querySelector(".password")
 const sendForm = document.querySelector(".sendform")
 const form = document.querySelector('.user-form')
+const loginFailed = document.querySelector('.failed')
+
+const savedToken = JSON.parse(window.localStorage.getItem('data'))
+
+// On créé un objet qui va nous permettre d'exploiter des données
+let saveData = {
+    authData: {
+        token : ""
+    }
+}
 
 // Création d'une fonction asynchrone
 async function dataUser(utilisateur, motDePasse) {
@@ -17,6 +27,16 @@ async function dataUser(utilisateur, motDePasse) {
     })
     const data = await reponse.json()
     console.log(data)
+
+    if (data.jwt != null) {
+        saveData[0] = {
+            token : data.jwt
+        }
+        saveObj()
+        window.location.href = "index.html"
+    } else {
+        loginFailed.style.display = "block"
+    }
 }
 
 // Formulaire pour entrer le nom
@@ -24,3 +44,11 @@ form.addEventListener('submit', (e) => {
     e.preventDefault()
     dataUser(emailField.value, passwordField.value)
 })
+
+function saveObj() {
+    window.localStorage.setItem('data', JSON.stringify(saveData))
+}
+
+if (savedToken[0].token != null) {
+    window.location.href = "index.html"
+}
