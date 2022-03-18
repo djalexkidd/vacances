eb08469f216cd67018c7f4454d86d5236b56e3f2
djalexkidd <alexpyr@outlook.fr>
2022-03-18 09:57:05 +0100

    Never Gonna Give You Up

--- public/js/home.js
+++ public/js/home.js
@@ -19,3 +19,37 @@ disconnectButton.addEventListener('click', (e) => {
 function saveObj() {
     window.localStorage.setItem('data', JSON.stringify(saveData))
 }
+
+/* EASTER EGG */
+var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b'
+};
+
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+var konamiCodePosition = 0;
+
+document.addEventListener('keydown', function(e) {
+    var key = allowedKeys[e.keyCode];
+    var requiredKey = konamiCode[konamiCodePosition];
+
+    if (key == requiredKey) {
+
+      konamiCodePosition++;
+
+      if (konamiCodePosition == konamiCode.length) {
+        activateCheats();
+        konamiCodePosition = 0;
+      }
+    } else {
+      konamiCodePosition = 0;
+    }
+});
+
+function activateCheats() {
+    window.location.href = "img/easteregg.webm"
+}
